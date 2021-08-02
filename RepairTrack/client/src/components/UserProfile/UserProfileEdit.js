ebd7fe92e34c195e14e41b29ae7c437a407c8495
Chadwell Clark <wcclark23@comcast.net>
2021-08-01 22:44:41 -0500

    moved userProfileEdit

---
+++ RepairTrack/client/src/components/UserProfile/UserProfileEdit.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { useHistory } from "react-router-dom";
+import { register } from "../../modules/authManager";
+// import logo1 from "../images/logo1.png";
+
+export default function Register() {
+  const history = useHistory();
+
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState();
+  //   const [password, setPassword] = useState();
+  //   const [confirmPassword, setConfirmPassword] = useState();
+
+  const registerClick = (e) => {
+    e.preventDefault();
+    if (password && password !== confirmPassword) {
+      alert("Passwords don't match. Do better.");
+    } else {
+      const userProfile = {
+        firstName,
+        lastName,
+        email,
+      };
+      register(userProfile, password).then(() => history.push("/"));
+    }
+  };
+
+  return (
+    <Form onSubmit={registerClick} className="form container-sm">
+      {/* <img className="logo1" src={logo1} alt="logo1" /> */}
+      <fieldset className="loginform">
+        <FormGroup>
+          <Label htmlFor="firstName">First Name</Label>
+          <Input
+            id="firstName"
+            type="text"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="lastName">Last Name</Label>
+          <Input
+            id="lastName"
+            type="text"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </FormGroup>
+        {/* <FormGroup>
+          <Label htmlFor="displayName">Display Name</Label>
+          <Input
+            id="displayName"
+            type="text"
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
+        </FormGroup> */}
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input
+            id="email"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        {/* <FormGroup>
+          <Label htmlFor="imageLocation">Profile Image URL</Label>
+          <Input
+            id="imageLocation"
+            type="text"
+            onChange={(e) => setImageLocation(e.target.value)}
+          />
+        </FormGroup> */}
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="confirmPassword">Confirm Password</Label>
+          <Input
+            id="confirmPassword"
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Button className="loginbutton">Register</Button>
+        </FormGroup>
+      </fieldset>
+    </Form>
+  );
+}
